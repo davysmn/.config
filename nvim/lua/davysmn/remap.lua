568e94230e6d4278c323d92475c50eb96ded4dec
davysmn <davy.simonin@gmail.com>
2023-03-05 19:35:46 +0100

    added alacritty setup

--- nvim/lua/davysmn/remap.lua
+++ nvim/lua/davysmn/remap.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>px", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pt", ":NvimTreeFocus<enter>")
+vim.keymap.set("n", "ƒ", ":m+<enter>")
+vim.keymap.set("n", "∫", ":m-2<enter>")
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)

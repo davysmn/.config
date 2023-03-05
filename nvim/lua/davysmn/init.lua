cd92e022a546db77311c509fbfdf64c83fc1ed04
Davy Simonin <davy.simonin@gmail.com>
2023-03-05 18:27:45 +0100

    .config files init

---
+++ nvim/lua/davysmn/init.lua
@@ -0,0 +1,3 @@
+require("davysmn.remap")
+require("davysmn.set")
+
---
+++ nvim/lua/davysmn/remap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>px", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pt", ":NvimTreeFocus<enter>")
